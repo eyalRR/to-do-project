b37a6c05cc060d0dc3647c71af9339ae34f1f6a2
Eyal <rabeyal123@gmail.com>
2021-12-21 18:38:22 +0200

    filtering

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 
 //Functions
@@ -53,4 +55,36 @@ function deleteCheck(e){
             todo.classList.toggle("completed");
         }
 
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+
+    todos.forEach(function(todo){
+        switch(e.target.value){
+
+            case "all":
+                console.log("all");
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                console.log("com");
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                } else{
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                console.log("un");
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "none";
+                } else{
+                    todo.style.display = "flex";
+                }
+                break;
+            default:
+                console.log("default");
+        }
+    })
 }
